162eae4b5a877654628a338ebb149b558177996c
ramziabboushi <you@example.com>
2021-05-03 14:47:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function setNewActive(el) {
+    //select all questions
+    var questionBodies = document.getElementsByClassName('question');
+    //remove active question
+    console.log(questionBodies)
+    // for (question of questionBodies) {
+        
+    // }
+}
